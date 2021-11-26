eedf941d6ca4465fedc20642b571cee7a6f68eec
rgooding <rgooding@users.noreply.github.com>
2021-11-26 08:59:30 +0000

    Fixed stream orders, randomised the timing and choice of stream

--- streams.py
+++ streams.py
@@ -13,23 +13,19 @@ def streams(tree, end_time):
         [3,4,5,6],
         [3,9,8,7],
         [3,10,11,12],
-        [3,15,14,13],
-        [3,16,17,18],
+        [3,13,14,15],
+        [3,18,17,16],
         [3,21,20,19],
         [3,22,23,24],
     ]
     
     tree.color = main_colour
 
-    si = 0
     while end_time == 0 or time() < end_time:
-        # sleep for between 0.3 and 0.7 seconds
-        #sleep(random.randrange(3, 7, 1) / 10)
-        sleep(1)
+        # sleep for between 0.5 and 1.2 seconds
+        sleep(random.randrange(5, 12, 1) / 10)
         
-        #stream = random.choice(streams)
-        #stream = streams[0]
-        stream = streams[si]
+        stream = random.choice(streams)
         for i in stream:
             tree[i].color = stream_colour
             sleep(0.05)
@@ -37,9 +33,6 @@ def streams(tree, end_time):
         for i in stream:
             tree[i].color = main_colour
             sleep(0.05)
-        si = si + 1
-        if si >= len(streams):
-            si = 0
 
 
 if __name__ == '__main__':
