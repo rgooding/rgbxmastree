8712ca3caba6c337fa6bb000698ac769c9dd1f40
rgooding <rgooding@users.noreply.github.com>
2021-12-03 23:01:27 +0000

    Changed Flasher logic

--- effects/brightness_waves.py
+++ effects/brightness_waves.py
@@ -1,4 +1,4 @@
-from time import time,sleep
+from time import time, sleep
 
 from colorzero import Color
 
@@ -14,21 +14,17 @@ def brightness_waves(tree, end_time):
         [1, 5, 8, 11, 14, 17, 20, 23],
         [0, 6, 7, 12, 15, 16, 19, 24],
     ]
-    # starting brightness of each row
-    brightnesses = [8, 4, 2, 1, 1, 1, 2, 4]
-    # brightnesses = [31, 16, 8, 4, 1, 4, 8, 16]
     min_brightness = 1
     max_brightness = 8
 
     fade_to(tree, Color('blue'))
     tree.updates_enabled = False
-    # tree.color = Color('blue')
 
     flashers = []
     i = 0
     for _ in rows:
         flashers.append(
-            Flasher(min_value=min_brightness, max_value=max_brightness, start_value=brightnesses[i], duration=2000))
+            Flasher(min_value=min_brightness, max_value=max_brightness, duration=2000, time_offset=i * 400))
         i += 1
 
     while end_time == 0 or time() < end_time:
@@ -36,7 +32,7 @@ def brightness_waves(tree, end_time):
         for row in rows:
             f = flashers[i]
             for n in row:
-                tree[n].brightness_int = f.value()
+                tree[n].brightness_int = int(f.value())
             i += 1
         tree.apply()
         sleep(0.01)
--- lib/flasher.py
+++ lib/flasher.py
@@ -2,30 +2,20 @@ from time import time
 
 
 class Flasher:
-    def __init__(self, min_value=1, max_value=8, start_value=1, duration=1000):
+    def __init__(self, min_value=1, max_value=8, duration=1000, time_offset=0):
         self.min = min_value
         self.max = max_value
-        self.start_value = start_value
         # flash duration in milliseconds (full cycle)
         self.dur = duration
-        self.start_time = time()
+        self.start_time = time() + (time_offset / 1000)
 
     def value(self):
         cycle_time = ((time() - self.start_time) * 1000) % self.dur
-        cycle_pos = cycle_time / self.dur
-        half_cycle_range = self.max - self.min
-        cycle_range = half_cycle_range * 2
-        start_offset = self.start_value - self.min
-        cycle_offset = ((cycle_range * cycle_pos) + start_offset) % cycle_range
-
-        if cycle_offset > half_cycle_range:
-            real_offset = half_cycle_range - (cycle_offset / 2)
+        half_dur = self.dur / 2
+        if cycle_time < half_dur:
+            # ascending
+            f = cycle_time / half_dur
         else:
-            real_offset = cycle_offset
-
-        real_value = self.min + real_offset
-        if real_value < self.min:
-            real_value = self.min
-        if real_value > self.max:
-            real_value = self.max
-        return int(real_value)
+            # descending
+            f = (half_dur - (cycle_time - half_dur)) / half_dur
+        return self.min + (f * (self.max - self.min))
