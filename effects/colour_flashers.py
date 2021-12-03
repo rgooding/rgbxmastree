807de6703cf7812c186593e549719485971b1014
rgooding <rgooding@users.noreply.github.com>
2021-12-03 23:02:36 +0000

    Added colour_flashers

---
+++ effects/colour_flashers.py
@@ -0,0 +1,56 @@
+from time import time, sleep
+
+from colorzero import Color, Hue
+
+from lib.fade import fade_to_multi
+from lib.flasher import Flasher
+
+
+# colour cycle each row separately using a Flasher to control the colour
+
+def colour_flashers(tree, end_time):
+    rows = [
+        [3],
+        [2, 4, 9, 10, 21],
+        [13, 18, 22],
+        [1, 5, 8, 11, 14, 17, 20, 23],
+        [0, 6, 7, 12, 15, 16, 19, 24],
+    ]
+    # starting colour for each row
+    colours = [
+        Color('red'),
+        Color('magenta'),
+        Color('yellow'),
+        Color('cyan'),
+        Color('blue'),
+    ]
+    min_shift = 0
+    max_shift = 90
+
+    initial_colours = [(0, 0, 0) for p in tree]
+    i = 0
+    for row in rows:
+        for n in row:
+            initial_colours[n] = colours[i]
+        i += 1
+
+    fade_to_multi(tree, initial_colours)
+
+    tree.updates_enabled = False
+
+    flashers = []
+    i = 0
+    for _ in rows:
+        flashers.append(Flasher(min_value=min_shift, max_value=max_shift, duration=2000))
+        i += 1
+
+    while end_time == 0 or time() < end_time:
+        i = 0
+        for row in rows:
+            f = flashers[i]
+            c = colours[i] + Hue(deg=f.value())
+            for n in row:
+                tree[n].color = c
+            i += 1
+        tree.apply()
+        sleep(0.01)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from effects.brightness_waves import brightness_waves
 from effects.brightness_waves2 import brightness_waves2
 from effects.colour_cycle import colour_cycle
+from effects.colour_flashers import colour_flashers
 from effects.colour_streams import colour_streams
 from effects.colour_waves import colour_waves
 from effects.colour_waves_vertical import colour_waves_vertical
@@ -16,6 +16,7 @@ if __name__ == '__main__':
     run_random([
         brightness_waves2,
         colour_cycle,
+        colour_flashers,
         colour_streams,
         colour_waves,
         colour_waves_vertical,
