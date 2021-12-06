dad2004092def0b860a55ecdc1afea90c54ac712
rgooding <rgooding@users.noreply.github.com>
2021-12-06 22:00:41 +0000

    Added colour_swap effect

---
+++ effects/colour_swap.py
@@ -0,0 +1,29 @@
+from time import time, sleep
+
+from colorzero import Color
+
+from lib.fade import fade_to_multi
+
+
+def colour_swap(tree, end_time):
+    colours = (Color('red'), Color('green'))
+
+    start_colours = [(0, 0, 0) for p in tree]
+    i = 0
+    for p in tree:
+        start_colours[i] = colours[i % 2]
+        i += 1
+    fade_to_multi(tree, start_colours)
+
+    # swap colours
+    j = 0
+    while end_time == 0 or time() < end_time:
+        sleep(3)
+        j += 1
+        j = j % 2
+        new_colours = [(0, 0, 0) for p in tree]
+        i = 0
+        for p in tree:
+            new_colours[i] = colours[(i + j) % 2]
+            i += 1
+        fade_to_multi(tree, new_colours, duration=0.25)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from effects.brightness_waves2 import brightness_waves2
 from effects.colour_cycle import colour_cycle
 from effects.colour_flashers import colour_flashers
 from effects.colour_streams import colour_streams
+from effects.colour_swap import colour_swap
 from effects.colour_waves import colour_waves
 from effects.colour_waves_vertical import colour_waves_vertical
 from effects.random_colours import random_colours
@@ -18,6 +19,7 @@ if __name__ == '__main__':
         colour_cycle,
         colour_flashers,
         colour_streams,
+        colour_swap,
         colour_waves,
         colour_waves_vertical,
         random_colours,
