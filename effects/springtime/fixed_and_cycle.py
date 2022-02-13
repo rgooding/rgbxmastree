44f316f07007bfcb308069171332b323e8d4e480
rgooding <rgooding@users.noreply.github.com>
2022-02-13 12:16:31 +0000

    Added pink_and_cycle effect

--- effects/springtime/fixed_and_cycle.py
+++ effects/springtime/fixed_and_cycle.py
@@ -4,27 +4,35 @@ from lib.fade import fade_to_multi
 from lib.sleeper import Sleeper
 from lib.tree import RGBXmasTree
 
+green = (0, 0.6, 0)
+pink = (0.8, 0.15, 0.3)
+
 
 def green_and_cycle(tree: RGBXmasTree, stop_func):
-    green = (0, 0.6, 0)
-    pink = (0.8, 0.15, 0.3)
+    fixed_and_cycle(tree, stop_func, green, pink)
 
-    pink_px = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23}
+
+def pink_and_cycle(tree: RGBXmasTree, stop_func):
+    fixed_and_cycle(tree, stop_func, pink, pink)
+
+
+def fixed_and_cycle(tree: RGBXmasTree, stop_func, fixed_colour, cycle_start_colour):
+    cycle_px = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23}
 
     start_colours = [(0.0, 0.0, 0.0) for _ in tree]
     i = 0
     for _ in tree:
-        if i in pink_px:
-            start_colours[i] = pink
+        if i in cycle_px:
+            start_colours[i] = cycle_start_colour
         else:
-            start_colours[i] = green
+            start_colours[i] = fixed_colour
         i += 1
     fade_to_multi(tree, start_colours)
 
     tree.updates_enabled = False
     s = Sleeper()
     while stop_func is None or not stop_func():
-        for i in pink_px:
+        for i in cycle_px:
             tree[i].color += Hue(deg=1)
         tree.apply()
         s.sleep(0.05)
--- springtime.py
+++ springtime.py
@@ -1,7 +1,7 @@
 from colorzero import Color
 
 from effects.colour_waves import colour_waves
-from effects.springtime.green_and_cycle import green_and_cycle
+from effects.springtime.fixed_and_cycle import green_and_cycle, pink_and_cycle
 from effects.springtime.pink_and_white import pink_and_white
 from lib.run_utils import run_random
 from lib.tree import RGBXmasTree
@@ -14,6 +14,7 @@ def colour_waves_spring(tree: RGBXmasTree, stop_func):
 if __name__ == '__main__':
     run_random([
         green_and_cycle,
+        pink_and_cycle,
         pink_and_white,
         colour_waves_spring
     ])
